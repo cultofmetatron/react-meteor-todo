2093c9cc8b66854252d4509031ee201553993a26
Peter de Croos <cultofmetatron@aumlogic.com>
2015-07-19 17:03:08 -0700

    basic working todo list

--- simple-todos.jsx
+++ simple-todos.jsx
@@ -1,10 +1,48 @@
+
 var Tasks = new Mongo.Collection('tasks');
 
+var Todo = React.createClass({
+  removeTodo: function() {
+    Tasks.remove({
+         _id: this.props.data._id
+       });
+  },
+  render: function() {
+    return <li><a href="#" onClick={this.removeTodo}>[X]</a> { this.props.data.text }</li> 
+  }
+});
+
+
+var TodoList = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData: function() {
+    return {
+      tasks: Tasks.find({}, {sort: {createdAt: -1}})
+    }
+  },
+  render: function() {
+    console.log(this.data.tasks)
+    return <ul>
+            { this.data.tasks.map(function(task) { return <Todo key={task.id} data={task}></Todo> }) }
+           </ul>;
+  }
+});
+
+
+
 if (Meteor.isClient) {
+  
   // counter starts at 0
   Template.body.helpers({
+    
     tasks: function () {
       return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+  
+  Template.todolist.helpers({
+    TodoList: function() {
+      return TodoList;
     }
   });
 
